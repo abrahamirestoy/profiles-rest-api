a4efcef14a89e8985429176e5ad595799bdea509
Abraham Islas <abrahamirestoy@gmail.com>
2019-08-08 20:12:22 +0200

    Added viewsets to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,15 @@
-#Apartado 8.30
-from django.urls import path
+#Apartado 8.30, Apartado 9.39 -> Add , include
+from django.urls import path, include
 from profiles_api import views
+#Apartado 9.39
+from rest_framework.routers import DefaultRouter
+
+#Apartado 9.39
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 urlpatterns = [
-    path('hello-view', views.HelloApiView.as_view())
+    path('hello-view/', views.HelloApiView.as_view()),
+    #Apartado 9.39
+    path('', include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 #Apartado 8.33
 from rest_framework import status
 from profiles_api import serializers
+#Apartado 9.38
+from rest_framework import viewsets
 
 #Apartado 8.29
 class HelloApiView(APIView):
@@ -12,6 +14,7 @@ class HelloApiView(APIView):
     #Apartado 8.33
     serializer_class = serializers.HelloSerializer
 
+    #Apartado 8.29
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
@@ -49,3 +52,58 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+
+#Apartado 9.38
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    #Apartado 9.41
+    serializer_class = serializers.HelloSerializer
+
+    #Apartado 9.38
+    def list(self, request):
+        """Return a hello  message"""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    #Apartado 9.41
+    def create(self, request):
+        """Create a new hello message."""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+
+        return Response({'http_method': 'DELETE'})
