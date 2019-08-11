eb2acf62ce80126f91b00ee326ef48c10361beea
Abraham Islas <abrahamirestoy@gmail.com>
2019-08-11 12:14:26 +0200

    test commit

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -162,3 +162,4 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
         serializer.save(user_profile=self.request.user)
+#Test changes
